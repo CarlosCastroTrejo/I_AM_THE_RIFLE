76bc447c3f2da36a948269a35f1fdc46d1ed7840
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2019-02-02 13:00:12 -0600

    First sketch of I AM THE RIFLE, enemies follows the tower, they doesn't avoid obstacles. A mesh navigator was added.

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    GameObject torre;
+    float distance;
+
+    void Explote() 
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        torre = GameObject.Find("Torre");
+        if (torre == null) 
+        {
+            Debug.Log("GameObject doesn't exist");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 dir = torre.transform.position - this.transform.position;
+        this.transform.position = this.transform.position + dir * Time.deltaTime*.1f;
+        Quaternion q = Quaternion.LookRotation(dir);
+        transform.rotation = q;
+        if (Vector3.Distance(torre.transform.position, this.transform.position) <=4.4f) 
+        {
+            Explote();
+        }
+
+
+    }
+}
